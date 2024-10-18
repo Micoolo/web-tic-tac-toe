36ab6f18c7d1057edf2573b41e306bcc10bf1248
Michał Kostański <m.kostanski3@gmail.com>
2024-10-18 20:49:18 +0200

    Added 4x4 game option

--- gameLogic.js
+++ gameLogic.js
@@ -4,19 +4,38 @@ const countO = document.getElementById("playerO");
 const countDraws = document.getElementById("draws");
 const fields = document.querySelectorAll(".field");
 let currentPlayer = 'X';
-let board = Array(9).fill(null);
 let gameActive = true;
+let boardSize, winningCombinations;
 
-const winningCombinations = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-];
+//checking if its a 3x3 or 4x4 board
+if(document.getElementById("board3x3")) {
+    boardSize = 3;
+    winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+} else if (document.getElementById("board4x4")) {
+    boardSize = 4;
+    winningCombinations = [
+        [0, 1, 2, 3],
+        [4, 5, 6, 7],
+        [8, 9, 10, 11],
+        [12, 13, 14, 15],
+        [0, 4, 8, 12],
+        [1, 5, 9, 13],
+        [2, 6, 10, 14],
+        [3, 7, 11, 15],
+        [0, 5, 10, 15],
+        [3, 6, 9, 12]
+    ];
+}
+let board = Array(boardSize * boardSize).fill(null);
 
 function fieldClick(event) {
     const index = event.target.dataset.index;
@@ -73,7 +92,7 @@ function highlightWinningFields() {
 }
 
 function restartGame() {
-    board = Array(9).fill(null);
+    board = Array(boardSize*boardSize).fill(null);
     fields.forEach(field => {
         field.textContent = '';
         field.classList.remove('x', 'o', 'winner', 'blank');
