329640b177c5bbe86ad3f6edcfce7fdd07651d9c
Michał Kostański <m.kostanski3@gmail.com>
2024-11-24 00:01:25 +0100

    Started animating example

--- exampleAnimation.js
+++ exampleAnimation.js
@@ -1,24 +1,49 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const images = document.querySelectorAll(".image");  // Pobieramy wszystkie obrazki
-    let currentIndex = 0;  // Indeks aktualnie wyświetlanego obrazka
-    images[0].classList.add("active");
-    function changeImage() {
-        // Ukrywamy obecny obrazek
-        setTimeout(function() {
-            images[currentIndex].classList.remove("active");
-        }, 500);
-        
+const images = document.querySelectorAll(".image");  // pobieranie obrazkow
+const startButton = document.getElementById("playButton");
+images[0].classList.add("active");
+let currentIndex = 0;
+let isRunning = null;
+let timeoutIds = [];
+let animationEnded = false;
 
-        // Przechodzimy do następnego obrazka (w pętli)
-        currentIndex = (currentIndex + 1) % images.length;
+function changeImage() {
+    setTimeout(function() {
+        images[currentIndex].classList.remove("active");
+    }, 500);
+    
+    currentIndex = (currentIndex + 1) % images.length;
 
-        // Pokazujemy nowy obrazek
+    setTimeout(function() {
+        images[currentIndex].classList.add("active");
+    }, 500);
+
+    if(currentIndex === (images.length - 1)) {
         setTimeout(function() {
-            images[currentIndex].classList.add("active");
-        }, 500);
+        animationEnded = true;
+        startButton.innerHTML = '<i class="fa-solid fa-rotate-right"></i>';
+        }, 1000);
     }
+}
+
+function startAnimation() {
+    timeoutIds.forEach(clearTimeout);
+    timeoutIds = [];
+
+    images.forEach((img) => img.classList.remove("active"));
+    currentIndex = 0;
+    images[currentIndex].classList.add("active");
 
     for(let i = 0; i < images.length; i++) {
-        setTimeout(changeImage, i * 3000);
-    } 
-});
+        if(animationEnded === true) {
+            setTimeout(() => {
+                const timeoutId = setTimeout(changeImage, i * 1000);
+                timeoutIds.push(timeoutId);
+            }, 500);
+        } else {
+            const timeoutId = setTimeout(changeImage, i * 1000);
+            timeoutIds.push(timeoutId);
+        }
+    }
+}
+
+startButton.addEventListener("click", startAnimation);
