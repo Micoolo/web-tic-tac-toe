5daba65435b51babecda4f8720f0e6a10c4fe354
Michał Kostański <m.kostanski3@gmail.com>
2024-11-24 16:22:03 +0100

    Finished example animations

--- exampleAnimation.js
+++ exampleAnimation.js
@@ -1,26 +1,36 @@
 const images = document.querySelectorAll(".image");  // pobieranie obrazkow
 const startButton = document.getElementById("playButton");
+const previousButton = document.getElementById("previousButton");
+const nextButton = document.getElementById("nextButton");
 images[0].classList.add("active");
 let currentIndex = 0;
-let isRunning = null;
+let isRunning = false;
 let timeoutIds = [];
 let animationEnded = false;
 
-function changeImage() {
-    setTimeout(function() {
-        images[currentIndex].classList.remove("active");
-    }, 500);
-    
-    currentIndex = (currentIndex + 1) % images.length;
+function animation() {
+    const previousIndex = currentIndex;
 
-    setTimeout(function() {
-        images[currentIndex].classList.add("active");
-    }, 500);
+    if (currentIndex < images.length - 1) {
+        setTimeout(function() {
+            images[previousIndex].classList.remove("active");
+        }, 400);
+        
+        currentIndex ++;
+
+        setTimeout(function() {
+            images[currentIndex].classList.add("active");
+        }, 0);
+    }
 
     if(currentIndex === (images.length - 1)) {
         setTimeout(function() {
         animationEnded = true;
+        isRunning = false;
+        updateButtonState();
         startButton.innerHTML = '<i class="fa-solid fa-rotate-right"></i>';
+        timeoutIds.forEach(clearTimeout);
+        timeoutIds = [];
         }, 1000);
     }
 }
@@ -28,22 +38,76 @@ function changeImage() {
 function startAnimation() {
     timeoutIds.forEach(clearTimeout);
     timeoutIds = [];
-
-    images.forEach((img) => img.classList.remove("active"));
-    currentIndex = 0;
-    images[currentIndex].classList.add("active");
-
+    startButton.innerHTML = '<i class="fa-solid fa-pause"></i>';
+    isRunning = true;
+    updateButtonState();
+    
     for(let i = 0; i < images.length; i++) {
-        if(animationEnded === true) {
-            setTimeout(() => {
-                const timeoutId = setTimeout(changeImage, i * 1000);
-                timeoutIds.push(timeoutId);
-            }, 500);
-        } else {
-            const timeoutId = setTimeout(changeImage, i * 1000);
-            timeoutIds.push(timeoutId);
-        }
+        const timeoutId = setTimeout(animation, i * 2000);
+        timeoutIds.push(timeoutId);
     }
 }
 
-startButton.addEventListener("click", startAnimation);
+function resetAnimation() {
+    stopAnimation();
+    animationEnded = false;
+    images.forEach((img) => img.classList.remove("active"));
+    currentIndex = 0;
+    images[currentIndex].classList.add("active"); 
+    startButton.innerHTML = '<i class="fa-solid fa-play"></i>';
+}
+
+function stopAnimation() {
+    isRunning = false;
+    updateButtonState();
+    timeoutIds.forEach(clearTimeout);
+    timeoutIds = [];
+    startButton.innerHTML = '<i class="fa-solid fa-play"></i>';
+}
+
+startButton.addEventListener("click", () => {
+    if (isRunning) {
+        stopAnimation();
+    } else if (animationEnded) {
+        resetAnimation();
+    } else if (!animationEnded) {
+        startAnimation();
+    }
+})
+
+function changeImage(index) {
+    const previousIndex = currentIndex;
+    setTimeout(function() {
+        images[previousIndex].classList.remove("active");
+    }, 250);
+
+    currentIndex = index;
+
+    setTimeout(function() {
+        images[currentIndex].classList.add("active");
+    }, 0);
+}
+
+previousButton.addEventListener("click", () => {
+    if (currentIndex > 0) {
+        const previousIndex = (currentIndex - 1);
+        changeImage(previousIndex);
+    }
+});
+
+nextButton.addEventListener("click", () => {
+    if (currentIndex < 3) {
+        const nextIndex = (currentIndex + 1);
+        changeImage(nextIndex);
+    }
+});
+
+function updateButtonState() {
+    if (isRunning) {
+        previousButton.classList.add("disabled");
+        nextButton.classList.add("disabled");
+    } else {
+        previousButton.classList.remove("disabled");
+        nextButton.classList.remove("disabled");
+    }
+}
